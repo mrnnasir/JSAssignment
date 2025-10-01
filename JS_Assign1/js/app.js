b7cffb44ffef89b15c671a4b163fc1c5bbbd6c58
mrnnasir <raihan80@gmail.com>
2025-10-01 12:09:56 +0200

    Created another file and updated codes

--- JS_Assign1/js/app.js
+++ JS_Assign1/js/app.js
@@ -1,8 +1,39 @@
+/*
 console.log('Hello World!');
-console.dir('Hello World!');
+console.dir('Hello World!'); //.dir will print everything within the quote
 
 console.log('This is line 1.');
 ;
 console.log('This is line 2.');
 console.log('This is line 3.'); console.log('This is line 4.');
 console.log('This is line 5.');
+
+
+This can be used multiline commenting
+ */
+
+const exampleString = 'Hello World!';
+console.log('Hello World!');
+
+var thisIsbadexample = 14;
+
+
+x = 's';
+y = 2;
+z = x+y;
+
+console.log(typeof(z));
+console.log(z);
+
+z = 'Trying to understand'
+console.log(z);
+
+let stringExample;
+let numberExample;
+numberExample = 4;
+
+stringExample = 'Hello World!';
+for (let i = 0; i < 10; i++) {
+  let stringE = 'Hello World!';
+}
+console.log(stringExample);
---
+++ JS_Assign2/js/app.js
@@ -0,0 +1,43 @@
+let numberOfBottles;
+numberOfBottoles = 10;
+
+const line1 = 'green bottles hanging on the wall'
+const line2 = 'and if one green bottle, should accidentally fall, there will be'
+
+for (let i = numberOfBottles; i >= 0;  i -= 1)
+{
+  if (i === 10) {
+    console.log(i);
+    console.log(line1);
+  }
+  else {
+    console.log(i);
+    console.log(line1);
+    console.log(line2);
+  }
+}
+
+//Instruction 1
+
+let speed = 72;
+ms = speed * 1000 / 3600;
+console.log(ms)
+
+//Instruction 2
+let minutes = 3;
+sec = minutes * 60;
+console.log(sec)
+
+
+//Instruction 3
+let x = 11;
+let y = 4;
+z = x/y
+console.log(z)
+
+
+//Instruction 4
+x = 11;
+y = 4;
+z = x % y
+console.log(z)
